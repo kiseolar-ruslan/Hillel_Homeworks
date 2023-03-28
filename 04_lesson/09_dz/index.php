c541227a9c2e5a90359f53eee0a844e3a7c05a71
ruslankrietsu <kretsu.rus@gmail.com>
2023-03-28 14:04:32 +0300

    Homework

--- 04_lesson/09_dz/index.php
+++ 04_lesson/09_dz/index.php
@@ -1,7 +1,41 @@
 <?php
-    include 'server.php';
-?>
 
+$vacancies = [
+    0 => ['id' => 1, 'title' => 'PHP Programmer', 'salary' => 2500, 'sector_id' => 1],
+    1 => ['id' => 2, 'title' => 'Designer', 'salary' => 3000, 'sector_id' => 1],
+    2 => ['id' => 3, 'title' => 'Finance Manager', 'salary' => 3500, 'sector_id' => 2],
+    3 => ['id' => 4, 'title' => 'Finance Director', 'salary' => 3500, 'sector_id' => 2],
+];
+
+$sectors = [
+    0 => ['id' => 1, 'title' => 'IT'],
+    1 => ['id' => 2, 'title' => 'Finance']
+];
+
+$fullVacancies = [];
+foreach ($vacancies as $key => $vacancy) {
+    foreach ($sectors as $sector) {
+        if ($vacancy['sector_id'] === $sector['id']) {
+            $vacancy['sector_title'] = $sector['title'];
+            unset($vacancy['sector_id']);
+            $fullVacancies[] = $vacancy;
+            continue 2;
+        }
+    }
+}
+
+
+//$keysArray = []; // пустой массив для ключей.
+
+//foreach ($fullVacancies as $fullVacancy) {
+//    foreach ($fullVacancy as $key =>  $item) {
+//        $keysArray[$key] = []; // добавляем ключи в пустой массив.
+//    }
+//}
+//
+//print_r($keysArray); // выводим ключи.
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,21 +48,27 @@
 </head>
 <body>
 <table>
-    <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Salary</th>
+            <th>Sector Title</th>
+        </tr>
+    </thead>
+    <tbody>
+    <!--data table-->
+    <?php
+    foreach ($fullVacancies as $fullVacancy) {
+        echo "<tr>";
+        echo "<td>" . $fullVacancy['id'] . "</td>";
+        echo "<td>" . $fullVacancy['title'] . "</td>";
+        echo "<td>" . $fullVacancy['salary'] . "</td>";
+        echo "<td>" . $fullVacancy['sector_title'] . "</td>";
+        echo "</tr>";
+    }
+    ?>
+    </tbody>
 </table>
 </body>
 </html>
