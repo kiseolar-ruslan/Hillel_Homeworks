5705019a37a40b945e157cd62a5ae92ee79da939
ruslankrietsu <kretsu.rus@gmail.com>
2023-03-30 20:02:19 +0300

    homework

--- 05_lesson/10_hw/server.php
+++ 05_lesson/10_hw/server.php
@@ -1,89 +1,51 @@
 <?php
+$fileDir = 'uploads';
 
 $personalData = $_POST;
-
 echo "<pre>";
 print_r($personalData);
 
 // Files data
-$fileDir = 'uploads';
 $files = $_FILES;
-
 echo "<pre>";
 print_r($files);
 
 
 $errorsList = [
-    "UPLOAD_ERR_OK" => "Ошибок не возникло, файл был успешно загружен на сервер.",
-    "UPLOAD_ERR_INI_SIZE" => "Размер принятого файла превысил максимально допустимый размер.",
-    "UPLOAD_ERR_FORM_SIZE" => "Размер загружаемого файла превысил максимальное значение.",
-    "UPLOAD_ERR_PARTIAL" => "Загружаемый файл был получен только частично.",
-    "UPLOAD_ERR_NO_FILE" => "Файл не был загружен.",
-    "UPLOAD_ERR_NO_TMP_DIR" => "Отсутствует временная папка.",
-    "UPLOAD_ERR_CANT_WRITE" => "Не удалось записать файл на диск.",
-    "UPLOAD_ERR_EXTENSION" => "Остановка загрузки, неизвестная ошибка."
+    0 => "Ошибок не возникло, файл был успешно загружен на сервер.",
+    1 => "Размер принятого файла превысил максимально допустимый размер.",
+    2 => "Размер загружаемого файла превысил максимальное значение.",
+    3 => "Загружаемый файл был получен только частично.",
+    4 => "Файл не был загружен.",
+    5 => "Отсутствует временная папка.",
+    6 => "Не удалось записать файл на диск.",
+    7 => "Остановка загрузки, неизвестная ошибка."
 ];
-
 
 if ($files) {
     foreach ($files as $file) {
         //count elements in array
         if (is_array($file['name'])) {
-            $length = count($file['name']);
-            for ($i = 0; $i < $length; $i++) {
-                if ($file['error'][$i]) {
-                    echo 'Error';
+            foreach ($file['name'] as $keys => $fil){
+                if ($file['error'][$keys]) {
+                    echo $errorsList[$file['error'][$keys]] . "<br>";
                     continue;
                 }
-//            mkdir($fileDir);
-                $fileTmp = $file['tmp_name'][$i];
-                $fileName = $file['name'][$i];
+                $fileTmp = $file['tmp_name'][$keys];
+                $fileName = $file['name'][$keys];
                 move_uploaded_file($fileTmp, "$fileDir/$fileName");
             }
         } else {
             if ($file['error']) {
-                echo 'Error';
+                echo $errorsList[$file['error']] . "<br>";
                 continue;
             }
-//            mkdir($fileDir);
             $fileTmp = $file['tmp_name'];
             $fileName = $file['name'];
             move_uploaded_file($fileTmp, "$fileDir/$fileName");
         }
     }
 }
-
-//if ($file['error'] === $errorsList[0]) {
-//    echo $errorsList[0][0];
-//}
-//if ($file['error'] === $errorsList[1]) {
-//    echo $errorsList[0][1];
-//}
-//if ($file['error'] === $errorsList[2]) {
-//    echo $errorsList[0][2];
-//}
-//if ($file['error'] === $errorsList[3]) {
-//    echo $errorsList[0][3];
-//}
-//if ($file['error'] === $errorsList[4]) {
-//    echo $errorsList[0][4];
-//}
-//if ($file['error'] === $errorsList[5]) {
-//    echo $errorsList[0][5];
-//}
-//if ($file['error'] === $errorsList[6]) {
-//    echo $errorsList[0][6];
-//}
-//if ($file['error'] === $errorsList[7]) {
-//    echo $errorsList[0][7];
-//}
-//if ($file['error'] === $errorsList[8]) {
-//    echo $errorsList[0][7];
-//}
-
-
-
-
 
 
 
